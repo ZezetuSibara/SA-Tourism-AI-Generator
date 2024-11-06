32c46a718e9a8a9cb89acaa1262623d199e69678
ZezetuSibara <123359014+ZezetuSibara@users.noreply.github.com>
2024-11-06 09:14:04 +0200

    Moved Files around

--- src/JavaScript.js
+++ src/JavaScript.js
@@ -1,5 +1,6 @@
 ﻿import axios from "axios";
 import Typewriter from "typewriter-effect";
+
 function displayCulture(response) {
     new Typewriter("#culture", {
         strings: response.data.answer,
@@ -15,7 +16,7 @@ function generateCulture(event) {
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "fa506aa8bdtd3d64a554291oa74a69fb";
     let context =
-        "You are a Tourist in South Africa and love to to experience different Cultures. Your mission is to generate a 4 line description in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Include a title to the Culture. Sign the Culture with 'SheCodes AI' inside a <strong> element at the end of the paragraph and NOT at the beginning";
+        "You are a Tourist in South Africa and love to experience different Cultures. Your mission is to generate a 4 line description in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Include a title to the Culture. Sign the Culture with 'SheCodes AI' inside a <strong> element at the end of the paragraph and NOT at the beginning";
     let prompt = `User instructions: Generate a Culture paragraph ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
@@ -23,7 +24,13 @@ function generateCulture(event) {
     cultureElement.classList.remove("hidden");
     cultureElement.innerHTML = `<div class="generating">⏳ Generating a paragraph about ${instructionsInput.value}</div>`;
 
-    axios.get(apiURL).then(displayCulture);
+    axios
+        .post(apiURL)
+        .then(displayCulture)
+        .catch(error => {
+            console.error("Error fetching data: ", error);
+            cultureElement.innerHTML = `<div class="error">An error occurred while generating the culture description.</div>`;
+        });
 }
 
 let cultureFormElement = document.querySelector("#culture-generator-form");
